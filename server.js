71ec1192b6ae406e7515229ac8a8cfb203e9d5e4
Sangkon Han <sigmadream@gmail.com>
2023-06-26 15:54:48 +0900

    init

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let userGoal = 'PNU Summer Sessions - Goal';
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <section>
+          <h2>PNU Summer Sessions - Goal</h2>
+          <h3>${userGoal}</h3>
+        </section>
+        <form action="/store-goal" method="POST">
+          <div class="form-control">
+            <label>Week01 Goal</label>
+            <input type="text" name="goal">
+          </div>
+          <button>Set Week01 Goal</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.post('/store-goal', (req, res) => {
+  const enteredGoal = req.body.goal;
+  console.log(enteredGoal);
+  userGoal = enteredGoal;
+  res.redirect('/');
+});
+
+app.listen(80);
